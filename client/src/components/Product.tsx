44b64afe32012fde9c966dc53d3a10a365a9c9ef
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-07-05 07:45:37 +0700

    fix ui product item

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -7,22 +7,16 @@ import './Product.scss'
 function Product({ data }: { data: ProductItem }) {
     return (
         <div className='product'>
-            {/* <div className='img-container'> */}
             <Link className='img-container' to={'/product/' + data.productID}>
                 <img src={baseIMG + 'products/' + data.images[0]} alt="" />
             </Link>
-            {/* </div> */}
             <div className='content-container'>
-                {/* <div className='top'> */}
                 <div className='name'>{data.productName}</div>
                 <div className='rate'>
                     <ProductRating rate={data.rating} />({data.ratingCount})
                 </div>
-                {/* </div> */}
-                {/* <div className='bottom'> */}
                 <div className='price'>{data.price.toLocaleString('vi') + 'đ'}</div>
                 <button>Add to cart</button>
-                {/* </div> */}
             </div>
         </div>
     )
