6536f916ddcbe8f266954e0f8b6e106201439992
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-07-11 13:46:03 +0700

    Add footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,6 +3,19 @@ import './Footer.scss'
 function Footer() {
     return (
         <div className='footer'>
+            <div>
+                <span>
+                    {"Online shoping @" + new Date().getFullYear()}
+                </span>
+                <a>Terms</a>
+                <a>Privacy</a>
+                <a>Security</a>
+                <a>Status</a>
+                <a>Docs</a>
+                <a>Contact</a>
+                <a>About</a>
+                <a>Terms</a>
+            </div>
         </div>
     )
 }
